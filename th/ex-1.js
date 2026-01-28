934a99ba24fb9f835c000fa13fcb6586409fc296
Sirapop Chanphui <srp073536@gmail.com>
2026-01-28 11:28:19 +0700

    feat: to complete all excersise

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,9 +1,39 @@
-const moveZeroes = function (nums) {
-  //Start coding here
+const moveZeroes1 = function (nums) {
+  let countZero = 0;
+  let newArray = [];
+  let pushZero = 0;
+  for (let num of nums) {
+    num === 0 ? countZero++ : newArray.push(num)
+  }
+  while (pushZero !== countZero) {
+    newArray.push(0);
+    pushZero++;
+  }
+  return newArray
 };
 
-const result1 = moveZeroes([0, 1, 0, 3, 12]);
+// two pointer
+const moveZeroes2 = function(nums) {
+  let index = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[index] = nums[i];
+      index++;
+    }
+  }
+
+  while (index < nums.length) {
+    nums[index] = 0;
+    index++;
+  }
+
+  return nums;
+};
+
+
+const result1 = moveZeroes1([0, 1, 0, 3, 12]);
 console.log(result1); // [1,3,12,0,0]
 
-const result2 = moveZeroes([0]);
+const result2 = moveZeroes1([0]);
 console.log(result2); // [0]
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,5 +1,29 @@
 const romanToInt = function (s) {
   //Start coding here
+  const romanMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const current = romanMap[s[i]]
+    const next = romanMap[s[i + 1]]
+    // console.log("current = ", current)
+    // console.log("next = ", next)
+    if (next && current < next) {
+      result -= current;
+    } else {
+      result += current
+    }
+  }
+  return result
 };
 
 const result1 = romanToInt("III"); // 3
